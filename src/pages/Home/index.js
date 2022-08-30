de176e17631d168aba0bd8ea9d3290c952f8c5b3
Hilton1 <josehilton02@gmail.com>
2022-08-30 00:37:28 -0300

    Status de erro no FormGroup e no Input

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,9 +4,6 @@ import {
   Container, InputSearchContainer, Header, ListContainer, Card,
 } from './styles';
 
-// import Modal from '../../components/Modal';
-import Loader from '../../components/Loader';
-
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
@@ -14,9 +11,6 @@ import trash from '../../assets/images/icons/trash.svg';
 export default function Home() {
   return (
     <Container>
-      <Loader />
-      {/* <Modal danger /> */}
-
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
       </InputSearchContainer>
