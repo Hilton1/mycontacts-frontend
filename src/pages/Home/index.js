c8f39582be62dd70746d8296c8ec4a930c115de6
Hilton1 <josehilton02@gmail.com>
2023-05-24 01:51:07 -0300

    Tratando os errors retornados pela API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -31,7 +31,7 @@ export default function Home() {
 
         setContacts(contactsList);
       } catch (error) {
-        console.log('errror', error);
+        console.log('Caiu no catch', error);
       } finally {
         setIsLoading(false);
       }
